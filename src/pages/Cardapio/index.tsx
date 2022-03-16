0161191427294caeb7f21200701bb26bcec976a6
Vitor Hugo Nogueira <5271@inb.gov.br>
2022-03-16 09:52:55 -0300

    .

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./cardapio.module.scss";
+
+export default function Cardapio(){
+    return(
+        <main>
+            <nav className={styles.menu}>
+                
+            </nav>
+        </main>
+    )
+}
